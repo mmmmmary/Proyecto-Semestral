209169f86082bb5a3486978a5e6345fa520a2b7e
GodPlan3 <cri.zamoraq@duocuc.cl>
2024-05-15 12:22:58 -0400

    commit432

---
+++ CosasPollo/scripts.js/scripts.js
@@ -0,0 +1,11 @@
+
+// const header = document.querySelector("header")
+// const nav = document.querySelector("nav");
+// const footer = document.querySelector("footer");
+
+// header.innerHTML = `<p class="text-light mb-0 p-2 fs-6 mr-auto">GroundZero.info@gmail.com</p>
+    // <p class="text-light mb-0 p-2 fs-6">+56 9 XXXX XXXX</p>
+// `
+
+
+
