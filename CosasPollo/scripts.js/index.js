19c91b4e69cee89e96b53dde229f66bde6645ae7
GodPlan3 <cri.zamoraq@duocuc.cl>
2024-05-16 22:50:46 -0400

    commit875

--- CosasPollo/scripts.js/index.js
+++ CosasPollo/scripts.js/index.js
@@ -1,24 +1,2 @@
 
 
-// api
-document.addEventListener('DOMContentLoaded', function() {
-    const apiKey = 'ba5f0aca1aa5090900b74b053ed1ba89'; // calveapii
-    const ciudad = 'Viña del mar';
-    const pais = 'CL';
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${apiKey}&units=metric`;
-
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Data:', data); // Imprimir toda la respuesta para diagnóstico
-            document.getElementById('temp').textContent = data.main.temp;
-            document.getElementById('wind').textContent = data.wind.speed;
-        })
-        .catch(error => console.error('Error fetching data: ', error));
-});
