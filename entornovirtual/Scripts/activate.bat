fdf2e02e618c3f287c7c7b2804f0a971d7e9b051
mmmmmary <ma.donosor@duocuc.cl>
2024-06-23 00:38:33 -0400

    aaaa

--- entornovirtual/Scripts/activate.bat
+++ entornovirtual/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\maryf\GroundZero\entornovirtual
+set VIRTUAL_ENV=C:\users\maryf\groundzero\entornovirtual
 
 if not defined PROMPT set PROMPT=$P$G
 
