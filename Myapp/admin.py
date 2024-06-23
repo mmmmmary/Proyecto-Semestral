700afbee8e4a8d1355d716e61a5c9f7b3554b27d
mmmmmary <ma.donosor@duocuc.cl>
2024-06-23 18:55:22 -0400

    assadas

--- Myapp/admin.py
+++ Myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Genero, Cliente, Artista
 # Register your models here.
+admin.site.register(Genero)
+admin.site.register(Cliente)
+admin.site.register(Artista)
